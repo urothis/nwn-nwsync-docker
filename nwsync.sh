b511ea9d559d22ceba092bceae968c30f598ed30
urothis <natecaple@gmail.com>
2018-10-24 20:22:54 +0000

    corrected sh

--- nwsync.sh
+++ nwsync.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./nwsync/ --description="My Cool Module Server Data V1" ./var/www/nwsync ./server/modules/*.mod
+./var/www/nwsync/update --description="My Cool Module Server Data V1" ./var/www/nwsync ./server/modules/*.mod
